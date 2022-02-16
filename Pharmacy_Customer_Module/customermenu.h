3cca38bb0edd00a20714ca7c02b9d7a7ef338d79
Somveer Kishnah <s.kishnah@uom.ac.mu>
2022-02-16 17:27:20 +0400

    worked on Main menu

---
+++ Pharmacy_Customer_Module/customermenu.h
@@ -0,0 +1,9 @@
+#ifndef CUSTOMER_MENU_FILE
+#define CUSTOMER_MENU_FILE
+
+int test()
+{
+    printf("this is a test");
+}
+
+#endif
---
+++ Pharmacy_Doctors_Module/doctors_Menu.h
@@ -0,0 +1,9 @@
+#ifndef DOCTORS_MENU_FILE
+#define DOCTORS_MENU_FILE
+
+int testDoctors()
+{
+    printf("this is the doctor's call");
+}
+
+#endif
