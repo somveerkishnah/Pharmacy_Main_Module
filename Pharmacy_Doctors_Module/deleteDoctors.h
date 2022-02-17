5fcfb62a8131bfc175bee1eb47de0407ef22f42b
amiira <amiiraemambokus@gmail.com>
2022-02-17 11:22:20 +0400

    updated

--- Pharmacy_Doctors_Module/deleteDoctors.h
+++ Pharmacy_Doctors_Module/deleteDoctors.h
@@ -1,29 +1,3 @@
-void read()
-{
-   FILE *ptr;
-    int c;
-    char name[10];
-    ptr=fopen("C:/Users/23059/OneDrive/Desktop/Amiira/Year 2 Sem 1/Software Engineering/Project/Pharmacy_Main_Module/doctorDetails.txt","r");
 
-   if(ptr==NULL)
-   {
-       printf("File not found\n");
-   }
-   else
-   {
-      printf("File opened in read mode");
-   }
-   while((c=fgetc(ptr))!=EOF)
-   {
-    printf("%c",c);
-   }
-   
-   ptr=fopen("C:/Users/23059/OneDrive/Desktop/Amiira/Year 2 Sem 1/Software Engineering/Project/Pharmacy_Main_Module/doctorDetails.txt,","w");
-   if(ptr==NULL)
-   {
-       printf("Error\n");
-       exit(1);
-   }
-}
    
 
