b6e4df4cbf46b71e63df6bea2129cdac40ecef85
amiira <amiiraemambokus@gmail.com>
2022-02-17 17:12:53 +0400

    file updated

--- Pharmacy_Doctors_Module/deleteDoctors.h
+++ Pharmacy_Doctors_Module/deleteDoctors.h
@@ -23,79 +23,59 @@
      
 }*/
 
-/*#include <stdio.h>
-#include<stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-#define FILENAME_SIZE 1024
-#define MAX_LINE 2048
-
-int delete ()
+void delete()
 {
-  // file handles for the original file and temp file
-  FILE *file, *temp;
-
-  // store the filename and temp filename
-  char filename[FILENAME_SIZE];
-  char temp_filename[FILENAME_SIZE];
-
-  // will store each line in the file, and the line to delete
-  char buffer[MAX_LINE];
-  int delete_line = 0;
-  
-  // have the user enter the filename, store it into the filename char array
-  printf("File: ");
-  scanf("%s", filename);
-  
-  // creates a temp filename "temp___filename.txt" where filename.txt is the 
-  // name of the file entered by the user by first copying the prefix temp____
-  // to temp_filename and then appending the original filename
-  strcpy(temp_filename, "temp____");
-  strcat(temp_filename, filename);
-  
-  // have the user enter the line number to delete, store it into delete_line
-  printf("Delete Line: ");
-  scanf("%d", &delete_line);
-  
-  // open the original file for reading and the temp file for writing
-  file = fopen(filename, "r");
-  temp = fopen(temp_filename, "w");
-  
-  // if there was a problem opening either file let the user know what the error
-  // was and exit with a non-zero error status
-  if (file == NULL || temp == NULL)
-  {
-    printf("Error opening file(s)\n");
-    return 1;
-  }
-  
-  // current_line will keep track of the current line number being read
-  bool keep_reading = true;
-  int current_line = 1;
-  do 
-  {
-    // stores the next line from the file into the buffer
-    fgets(buffer, MAX_LINE, file);
-   
-    // if we've reached the end of the file, stop reading from the file, 
-    // otherwise so long as the current line is NOT the line we want to 
-    // delete, write it to the file
-    if (feof(file)) keep_reading = false;
-    else if (current_line != delete_line)
-      fputs(buffer, temp);
-    
-    // keeps track of the current line being read
-    current_line++;
-  
-  } while (keep_reading);
-  
-  // close our access to the files
-  fclose(file);
-  fclose(temp);
-  
-  // delete the original file, give the temp file the name of the original file
-  remove(filename);
-  rename(temp_filename, filename);
-return 0;
-}*/
+  FILE *fp1, *fp2;
+        //consider 40 character string to store filename
+        char filename[40];
+        char c;
+        int del_line, temp = 1;
+        //asks user for file name
+        printf("Enter file name: ");
+        //receives file name from user and stores in 'filename'
+        scanf("%s", filename);
+        //open file in read mode
+        fp1 = fopen(filename, "r");
+        c = getc(fp1);
+        //until the last character of file is obtained
+        while (c != EOF)
+        {
+          printf("%c", c);
+          //print current character and read next character
+          c = getc(fp1);
+        }
+        //rewind
+        rewind(fp1);
+        printf(" \n Enter line number of the line to be deleted:");
+        //accept number from user.
+        scanf("%d", &del_line);
+        //open new file in write mode
+        fp2 = fopen("copy.c", "w");
+        c = getc(fp1);
+        while (c != EOF) {
+          c = getc(fp1);
+          if (c == '\n')
+          temp++;
+          //except the line to be deleted
+          if (temp != del_line)
+          {
+            //copy all lines in file copy.c
+            putc(c, fp2);
+          }
+        }
+        //close both the files.
+        fclose(fp1);
+        fclose(fp2);
+        //remove original file
+        remove(filename);
+        //rename the file copy.c to original name
+        rename("copy.c", filename);
+        printf("\n The contents of file after being modified are as  follows:\n");
+        fp1 = fopen(filename, "r");
+        c = getc(fp1);
+        while (c != EOF) {
+            printf("%c", c);
+            c = getc(fp1);
+        }
+        fclose(fp1);
+}
