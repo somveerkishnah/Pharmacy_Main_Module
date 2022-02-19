cbfec0afae5c623a8f2c3cceb9138e62382ccd08
Vaishnavi Dookheet <dookheetvaishnavi@gmail.com>
2022-02-19 17:16:38 +0400

    added in add_supplier

--- Pharmacy_Suppliers_Module/add_Supplier.h
+++ Pharmacy_Suppliers_Module/add_Supplier.h
@@ -1,4 +1,38 @@
 #ifdef ADD_SUPPLIER_FILE
 #define ADD_SUPPLIER_FILE
 
+void addsupplier()
+{
+    int num;
+   
+     char dataToAppend[BUFFER_SIZE];
+
+    printf("Enter the number of suppliers that you want to add info:\n");
+    scanf("%d",&num);
+
+    FILE *fptr;
+    fptr=fopen("supplier_Details.txt","a");
+
+    if(fptr==NULL)
+    {
+        printf("Error!");
+        exit(EXIT_FAILURE);
+    }
+
+
+    
+   printf("Enter data to append\n");
+   fflush(stdin);
+   fgets(dataToAppend,BUFFER_SIZE,stdin);
+   fputs(dataToAppend,fptr);
+
+
+   fptr=freopen("C:/Users/MAMTA/OneDrive/Desktop/vaishsupplier/SupplierInfo.txt","r",fptr);
+   printf("\nSuccessfully appended data\n");
+   readFile(fptr);
+   printf("\n");
+
+    fclose(fptr);
+}
+
 #endif
