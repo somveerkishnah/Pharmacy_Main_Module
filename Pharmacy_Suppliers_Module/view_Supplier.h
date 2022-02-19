193f806d546c0f1200f69b26276fded73523890c
Avishna <bootunavishna5@gmail.com>
2022-02-19 17:13:32 +0400

    Updated View

--- Pharmacy_Suppliers_Module/view_Supplier.h
+++ Pharmacy_Suppliers_Module/view_Supplier.h
@@ -6,13 +6,6 @@ void viewsupplier()
 {
     FILE * ptr;
     int c;
-<<<<<<< HEAD
-
-    ptr=fopen("SupplierInfo.txt","r");
-
-    ptr=fopen("supplier_Details.txt","r");
-=======
->>>>>>> 01868c0c4489ceb0c36296d0a6c49123eef6ffd0
 
     ptr=fopen("supplier_Details.txt","r");
 
