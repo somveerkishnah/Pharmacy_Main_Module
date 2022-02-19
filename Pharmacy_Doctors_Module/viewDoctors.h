879f8c40cf12f87bb462b374fa6f5cc684cd0fe2
Aashi Jaulim <aashi.jaulim@gmail.com>
2022-02-19 15:41:50 +0400

    updated viewDoctors.h

--- Pharmacy_Doctors_Module/viewDoctors.h
+++ Pharmacy_Doctors_Module/viewDoctors.h
@@ -1,4 +1,22 @@
 #ifdef VIEW_DOCTOR_FILE
 #define VIEW_DOCTOR_FILE
-
+void read()
+{
+   FILE *ptr;
+    int c;
+    ptr=fopen("DoctorsDetailsNew.txt", "r");
+    if(ptr==NULL)
+    {
+        printf("\nFile not found\n");
+    }
+    else
+    {
+        printf("\nThe file contains:\n");
+    }
+    while((c=fgetc(ptr))!=EOF)
+    {
+        printf("%c", c);
+    }
+    fclose(ptr);
+}
 #endif
