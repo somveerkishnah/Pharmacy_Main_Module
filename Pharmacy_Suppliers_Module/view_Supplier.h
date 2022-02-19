0f0932077942f25579462921b14b5034f73d756f
Vaishnavi Dookheet <dookheetvaishnavi@gmail.com>
2022-02-19 16:49:20 +0400

    Vaish added in viewsupplier.h

--- Pharmacy_Suppliers_Module/view_Supplier.h
+++ Pharmacy_Suppliers_Module/view_Supplier.h
@@ -1,4 +1,26 @@
-#ifdef VIEW_SUPPLIER_FILE
-#define VIEW_SUPPLIER_FILE
+#ifndef SUPPLIER_FILE
+#define SUPPLIER_FILE
+#include<stdlib.h>
+#define BUFFER_SIZE 1000
 
+void viewsupplier()
+{
+    FILE * ptr;
+    int c;
+    ptr=fopen("SupplierInfo.txt","r");
+
+    if(ptr == NULL)
+    {
+        printf("File not found\n");
+    }
+    else
+    {
+        printf("File opened in read mode");
+    } 
+    while((c=fgetc(ptr))!=EOF)
+    {
+        printf("%c",c);
+    }
+
+}
 #endif
