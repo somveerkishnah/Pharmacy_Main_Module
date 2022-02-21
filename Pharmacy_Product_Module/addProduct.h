ec75fc5dcaa777118dc70401d88d15003e88da91
Dantev25 <oomargingo@gmail.com>
2022-02-21 16:57:14 +0400

    Fixed formatting in addProducts

--- Pharmacy_Product_Module/addProduct.h
+++ Pharmacy_Product_Module/addProduct.h
@@ -27,7 +27,7 @@ void addProducts()
         printf("Enter quantity: ");
         scanf("%d",&quantity);
 
-        fprintf(fptr,"\n%s \t\t%d \t\t%d\n",productName,cost,quantity);
+        fprintf(fptr,"%2s%10d%10d\n",productName,cost,quantity);
     }
     fclose(fptr);
 }
