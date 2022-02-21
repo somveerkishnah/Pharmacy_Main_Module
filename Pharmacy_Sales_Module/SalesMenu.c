4c2e7b8b439cb9af01ad04dcc19a64b8b32f8363
praveer <praveer.seetahul1@umail.uom.ac.mu>
2022-02-21 16:17:11 +0400

    sales menu(salesmenu.c) updated

--- Pharmacy_Sales_Module/SalesMenu.c
+++ Pharmacy_Sales_Module/SalesMenu.c
@@ -10,9 +10,42 @@
 
 int main()
 {
-  Add();
-  Delete();
-  viewSales();
-}
+  int choice;
+    printf("1.View sales\n");
+    printf("2.Add new Sales\n");
+    printf("3.Delete sales\n");
+    printf("0.exit");
+    do{
+        printf("\nEnter your choice :");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            {
+               viewSales();
+               break;
+            }
+            case 2:
+            {
+              Add();
+              break; 
+            }
+            case 3:
+            {
+              Delete();
+              break;
+            }
+            default:
+            {
+                printf("Invalid choice");
+            }
 
+        }
+        
+      }while(choice !=0);
+
+}
 #endif
+
+
+
