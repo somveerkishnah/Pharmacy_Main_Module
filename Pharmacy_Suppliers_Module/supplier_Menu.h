15782c6b5e6aef8d24af72310027ce13cc57351c
Yashi <yashimunogee@gmail.com>
2022-02-21 14:18:53 +0400

    error

--- Pharmacy_Suppliers_Module/supplier_Menu.h
+++ Pharmacy_Suppliers_Module/supplier_Menu.h
@@ -1,29 +1,12 @@
 #include "view_Supplier.h"
-<<<<<<< HEAD
-//#include "add_Supplier.h"
-=======
-#include "add_Supplier.h"
->>>>>>> bf4ba23be7f26897858acb7eeab1879152b04a03
-#include "delete_Supplier.h"
- 
-void output(float);
- 
-int menu()
-{
-    float result;
-  
-  int choice, num;
- 
-    printf("\n\n\n\n\n\t~~~~\tWELCOME TO SUPPLIERS MENU\t~~~~\t\n\n\n\n\n");
-   
       printf("\nChoose from the following:\n");
       printf("1. View Supplier\n");
       printf("2. Delete line\n");
-<<<<<<< HEAD
-=======
+
+
       printf("3. Add supplier\n");
  
->>>>>>> bf4ba23be7f26897858acb7eeab1879152b04a03
+
  
       scanf("%d",&choice);
  
@@ -34,18 +17,10 @@ int menu()
             break;
          }
           case 2: {
-             deleteSupplier();
-<<<<<<< HEAD
-             break;
-         }
-=======
-            break;
-         }
           case 3: {
              addsupplier();
             break;
           }
->>>>>>> bf4ba23be7f26897858acb7eeab1879152b04a03
          
          
           default:
