8207440128829c4c42b6f02a9f92b3cca9f0fe4f
Navish Khadooa <navish.khadooa1@umail.uom.ac.mu>
2022-02-21 14:47:21 +0400

    text file updated

--- Pharmacy_Sales_Module/tempCodeRunnerFile.c
+++ Pharmacy_Sales_Module/tempCodeRunnerFile.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-<<<<<<< HEAD
 
-=======
-#include <string.h>
-#include <stdbool.h>
- 
->>>>>>> 27d51bd413b59f3eb6c03409eebb84656e63626c
 int main()
 {
-   int num,n;
-   char name[100];
-   FILE *fptr;
-    
-   //This code appends to the text file
-   fptr = fopen("SalesDetail.txt","a");
+    FILE *fileptr1, *fileptr2;
+    char filename[40];
+    char ch;
+    int delete_line, temp = 1;
 
-   if(fptr == NULL)
-   {
-      printf("Error!");
-      exit(1);
-   }
-
-   printf("Enter item name:");
-   scanf("%s", &name);
-   fprintf(fptr, "%s       ",name);
-
-   printf("Enter Quantity Sold: ");
-   scanf("%d\n", &num);
-   fprintf(fptr, "%d\n",num);
-
-   fclose(fptr);
-
-
-   return 0;
+    printf("Enter file name: ");
+    scanf("%s", filename);
+    //open file in read mode
+    fileptr1 = fopen(filename, "r");
+    ch = getc(fileptr1);
+   while (ch != EOF)
+    {
+        printf("%c", ch);
+        ch = getc(fileptr1);
+    }
+    //rewind
+    rewind(fileptr1);
+    printf(" \n Enter line number of the line to be deleted:");
+    scanf("%d", &delete_line);
+    //open new file in write mode
+    fileptr2 = fopen("replica.c", "w");
+    ch = 'A';
+    while (ch != EOF)
+    {
+        ch = getc(fileptr1);
+        //except the line to be deleted
+        if (temp != delete_line)
+        {
+            //copy all lines in file replica.c
+            putc(ch, fileptr2);
+        }
+        if (ch == '\n')
+        {
+            temp++;
+        }
+    }
+    fclose(fileptr1);
+    fclose(fileptr2);
+    remove(filename);
+    //rename the file replica.c to original name
+    rename("replica.c", filename);
+    printf("\n The contents of file after being modified are as follows:\n");
+    fileptr1 = fopen(filename, "r");
+    ch = getc(fileptr1);
+    while (ch != EOF)
+    {
+        printf("%c", ch);
+        ch = getc(fileptr1);
+    }
+    fclose(fileptr1);
+    return 0;
 }
