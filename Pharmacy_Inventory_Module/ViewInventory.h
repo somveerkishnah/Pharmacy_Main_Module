849efea1cbb7734bb6dfdcb10e711fb71b5a915d
Jess <hsjess07@gmail.com>
2022-02-21 19:59:11 +0400

    Inventory_menu created and ViewInventory edited

---
+++ Pharmacy_Inventory_Module/ViewInventory.h
@@ -0,0 +1,38 @@
+#ifndef VIEW_INVENTORY_H_INCLUDED
+#define VIEW_INVENTORY_H_INCLUDED
+void inventoryView(){
+    FILE *ptr;
+    FILE *ptr2;
+    FILE *ptr3;
+    ptr=fopen("Pharmacy_Sales_Module/SalesDetail.txt","r");
+    ptr2=fopen("Pharmacy_Orders_Module/orders.txt","r");
+    ptr3=fopen("Pharmacy_Inventory_Module/Inventory.txt","w");
+
+    char data[]="SALES TXT:";
+    if(strlen(data)>0){
+        fputs(data,ptr3);
+        fputs("\n",ptr3);
+    }
+   
+    char c;
+    while((c=fgetc(ptr))!= EOF){
+        fputc(c,ptr3);
+    }
+    fclose(ptr);
+    printf("\n Content of SalesDetail.txt copied to Inventory.txt\n");
+    char data2[]="ORDERS TXT:";
+    if(strlen(data2)>0){
+        fputs("\n",ptr3);
+        fputs(data2,ptr3);
+        fputs("\n",ptr3);
+    }
+    
+    char c2;
+    while((c2=fgetc(ptr2))!= EOF){
+        fputc(c2,ptr3);
+    }
+    printf(" Content of orders.txt copied to Inventory.txt\n\n");
+    fclose(ptr2);
+    fclose(ptr3);
+}
+#endif
