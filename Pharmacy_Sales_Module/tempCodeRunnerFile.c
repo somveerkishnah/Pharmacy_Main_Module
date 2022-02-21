25330c89cd536c367f393568191a41d975ce392b
praveer <praveer.seetahul1@umail.uom.ac.mu>
2022-02-21 16:04:42 +0400

    sales menu updated

---
+++ Pharmacy_Sales_Module/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+#ifndef SALES_MENU_FILE
+#define SALES_MENU_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Sales_ADD.h"
+#include "Sales_Delete.h"
+#include "Sales_Read.h"
+
+
+int main()
+{
+  Add();
+  Delete();
+  viewSales();
+}
+
+#endif
