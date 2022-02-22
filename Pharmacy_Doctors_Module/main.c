5e92b173fdb4ad7de473f77df7c0294f2957289b
amiira <amiiraemambokus@gmail.com>
2022-02-22 08:22:50 +0400

    Worked on main.c

--- Pharmacy_Doctors_Module/main.c
+++ Pharmacy_Doctors_Module/main.c
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-//#include"deleteDoctors.h"
-#include"viewDoctors.h"
-//#include"addDoctors.h"
-int main() 
-{
-  //delete();
-  view();
-  //add();
-}
-
-
-
-=======
 #include<stdio.h>
 #include<stdlib.h>
 #include"doctors_Menu.h"
@@ -20,4 +5,4 @@ int main()
 {
   menu();
 }
->>>>>>> ea975634888169a7cf57342e25e882221d83df8f
+
